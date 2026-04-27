437d1884f9573aa46f2fec7d69a7164596378b47
mangofizk <mangofisk@hotmail.com>
2026-04-27 14:53:46 +0200

    interactions between player and enemy

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private float startingFirerate;
     public static float fireRate;
+    public static int gundamage;
     private float timer;
+
 
     [SerializeField] private int startingHealth;
     public static float health;
+
+    public GameObject[] enemies;
+
+    [SerializeField] Slider healthbar;    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +22,8 @@ public class PlayerManager : MonoBehaviour
         timer = 0;
         fireRate = startingFirerate;
         health = startingHealth;
+
+        setHealthBar();
     }
 
     // Update is called once per frame
@@ -30,11 +39,41 @@ public class PlayerManager : MonoBehaviour
 
     public void shoot()
     {
+       
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
         Debug.Log("bang");
+
+        GameObject nearestEnemy = enemies[0];
+        float distanceToNearest = Vector3.Distance(transform.position, nearestEnemy.transform.position);
+
+        for (int i = 1; i < enemies.Length; i++)
+        {
+            float distanceToCurrent = Vector3.Distance(transform.position, enemies[i].transform.position);
+
+            if (distanceToCurrent < distanceToNearest)
+            {
+                nearestEnemy = enemies[i];
+                distanceToNearest = distanceToCurrent;
+            }
+        }
+
+        nearestEnemy.GetComponent<enemyManager>().dealdamage(gundamage);
     }
 
-    public void dealDamage(int damage)
+    public void takeDamage(int damage)
     {
         health -= damage;
+        setHealthBar();
+
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void setHealthBar()
+    {
+        healthbar.value = health / startingHealth;
+
     }
 }
